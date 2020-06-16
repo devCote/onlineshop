382fb97ef81e6961d2e4793e6f950b4f8ccc7aa8
DmitriyBakhanenko <bahanenko@gmail.com>
2020-06-16 19:51:28 +0300

    set react router and set page routs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage';
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path={`/shop/hats`} component={HatsPage} />
+      </Switch>
     </div>
   );
 }
