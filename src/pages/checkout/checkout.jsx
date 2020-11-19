a7514b3014fb614921fefb352fd67fa206a413af
Dmitriy Bakhanenko <bahanenko@gmail.com>
2020-11-19 21:50:52 +0200

    integrated stripe payment method and updated title and favicon icon

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './checkout.styles.scss';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from 'react'
+import './checkout.styles.scss'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import {
   selectCartItems,
   selectCartTotal,
-} from '../../redux/cart/cart.selectors';
-import CheckoutItem from '../../components/checkout-item/checkout-item';
+} from '../../redux/cart/cart.selectors'
+import CheckoutItem from '../../components/checkout-item/checkout-item'
+import StripeCheckoutBtn from '../../components/stripe-btn/stripe-btn.component'
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -33,12 +34,18 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+    </div>
+    <StripeCheckoutBtn price={total} className="stripe-btn" />
   </div>
-);
+)
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal,
-});
+})
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default connect(mapStateToProps)(CheckoutPage)
